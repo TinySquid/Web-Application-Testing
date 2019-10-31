81f7b06a69515e75ef625374de360349157dca09
Michael Nunes <miketnunes@gmail.com>
2019-10-31 10:37:58 -0700

    Build out initial scaffold for dashboard

---
+++ baseball/src/components/Dashboard.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+import Display from './Display';
+
+export default class Dashboard extends Component {
+  state = {
+    balls: 0,
+    strikes: 0
+  }
+
+  strike = () => {
+
+  }
+
+  ball = () => {
+
+  }
+
+  foul = () => {
+
+  }
+
+  hit = () => {
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
