2d53b8c222efe3fa802d22520af7b5d19057ef9b
Michael Nunes <miketnunes@gmail.com>
2019-10-31 13:00:24 -0700

    Implement MVP features

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -2,9 +2,13 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import App from './App';
 
-afterEach(rtl.cleanup);
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  rtl.render(<App />, div);
+test('it renders without crashing', () => {
+  rtl.render(<App />);
 });
+
+// afterEach(rtl.cleanup);
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   rtl.render(<App />, div);
+// });
--- baseball/src/components/Dashboard.jsx
+++ baseball/src/components/Dashboard.jsx
@@ -9,26 +9,55 @@ export default class Dashboard extends Component {
   }
 
   strike = () => {
-
+    const strikeCount = this.state.strikes;
+    //balls and strikes reset to 0 when a player reaches 3 strikes
+    if (strikeCount === 2) {
+      this.resetBoard();
+    } else {
+      this.setState({ strikes: strikeCount + 1 });
+    }
   }
 
   ball = () => {
-
+    const ballCount = this.state.balls;
+    //balls and strikes reset to 0 when a player reaches 4 balls.
+    if (ballCount === 3) {
+      this.resetBoard();
+    } else {
+      this.setState({ balls: ballCount + 1 });
+    }
   }
 
   foul = () => {
-
+    const strikeCount = this.state.strikes;
+    //a `foul` increases strikes up to 2
+    //With two strikes a foul has no effect, count stays at 2 strikes.
+    if (strikeCount < 2) {
+      this.setState({ strikes: strikeCount + 1 });
+    }
   }
 
+  //balls and strikes reset to 0 when a `hit` is recorded.
   hit = () => {
+    this.resetBoard();
+  }
 
+  //Function to reset for next player 'at bat' 
+  resetBoard = () => {
+    this.setState({ balls: 0, strikes: 0 });
   }
 
   render() {
     return (
-      <div>
-
-      </div>
+      <>
+        <Display balls={this.state.balls} strikes={this.state.strikes} />
+        <div className="buttons">
+          <button onClick={this.strike}>Strike</button>
+          <button onClick={this.ball}>Ball</button>
+          <button onClick={this.foul}>Foul</button>
+          <button onClick={this.hit}>Hit</button>
+        </div>
+      </>
     )
   }
 }
